a039e599d2386ef4b2977f8047a263710b87e63a
shin-muja <shin970712@gmail.com>
2024-11-13 17:17:36 +0900

    test추가

--- demo/src/test/java/com/example/test/Test.java
+++ demo/src/test/java/com/example/test/Test.java
@@ -3,13 +3,9 @@ package com.example.test;
 public class Test {
 	public void test() {
 		System.out.println("시험");
-<<<<<<< HEAD
-		
-		System.out.println("신동국");
-=======
 		System.out.println("우수민");
 		System.out.println("고민규");
->>>>>>> 5ee9f7b660990a75aac8a256a2571814c90d8ae0
+		System.out.println("신동국");
 	}
 }
 
